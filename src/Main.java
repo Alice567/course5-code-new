9e79a8b4004a20144a7a002879bbba3ce8d5f107
Alice567 <alisa.muresan567@yahoo.com>
2023-01-03 20:51:52 +0200

    new line

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Something that is awesome!");
         System.out.println("Hello world!");
+
     }
 }
