514d34cf7e04c8ae1cd79b1b94fedcc9d8c02b8a
Alice567 <alisa.muresan567@yahoo.com>
2023-02-27 13:09:56 +0200

    change from "Hello world" to "test test test"

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Something that is awesome!");
-        System.out.println("Hello world!");
+        System.out.println("test test test!");
 
     }
 }
